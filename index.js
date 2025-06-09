1deec88c4dc19e6a0483aeec2918bf4ee643bd52
parsamoloody <parsaprogramerjs@gmail.com>
2025-06-09 15:52:55 +0330

    fix cors issue

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-import env from 'dotenv'
-import express from 'express'
-const app = express();
-import router from './routes/chatRoutes.js'
+import env from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import router from './routes/chatRoutes.js';
 import logger from './utils/logger.js';
-import cors from 'cors'
-env.config(cors())
+
+env.config();
+const app = express();
+
+app.use(cors());
 app.use(express.json());
+
 app.use("/api", router);
 
 const PORT = process.env.PORT || 3000;
